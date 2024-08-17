71bb248150bceb4f9c36aa2b0d03314f3b2cd71f
ramikg <72725910+ramikg@users.noreply.github.com>
2024-08-18 00:51:18 +0300

    Improve sanity type tests

--- test/types/mapper.test.ts
+++ test/types/mapper.test.ts
@@ -2,10 +2,11 @@ import {describe, expect, test} from "tstyche";
 import {mapping} from "cassandra-driver";
 import {testMapper, TestRow} from "../../types/generated";
 
-describe("Type tests", () => {
+describe("Sanity type tests", () => {
     test("mapper.find()", () => {
         expect(testMapper.find({partitionKey1: ''})).type.toRaiseError();
         expect(testMapper.find({partitionKey1: '', clustering1: ''})).type.toRaiseError();
+        expect(testMapper.find({partitionKey1: '', partitionKey2: 0})).type.toRaiseError();
 
         expect(testMapper.find({partitionKey1: '', partitionKey2: ''})).type.toBe<Promise<mapping.Result<TestRow>>>;
         expect(testMapper.find({partitionKey1: '', partitionKey2: '', clustering1: ''})).type.toBe<Promise<mapping.Result<TestRow>>>;
@@ -15,6 +16,7 @@ describe("Type tests", () => {
         expect(testMapper.get({partitionKey1: '', partitionKey2: ''})).type.toRaiseError();
         expect(testMapper.get({partitionKey1: '', partitionKey2: '', clustering1: ''})).type.toRaiseError();
         expect(testMapper.get({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: '', extraColumn1: ''})).type.toRaiseError();
+        expect(testMapper.get({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: 0})).type.toRaiseError();
 
         expect(testMapper.get({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: ''})).type.toBe<Promise<TestRow | null>>;
     });
@@ -22,6 +24,7 @@ describe("Type tests", () => {
     test("mapper.insert()", () => {
         expect(testMapper.insert({partitionKey1: '', partitionKey2: ''})).type.toRaiseError();
         expect(testMapper.insert({partitionKey1: '', partitionKey2: '', clustering1: ''})).type.toRaiseError();
+        expect(testMapper.insert({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: 0})).type.toRaiseError();
 
         expect(testMapper.insert({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: ''})).type.toBe<Promise<mapping.Result<TestRow>>>;
         expect(testMapper.insert({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: '', extraColumn1: ''})).type.toBe<Promise<mapping.Result<TestRow>>>;
@@ -30,6 +33,7 @@ describe("Type tests", () => {
     test("mapper.update()", () => {
         expect(testMapper.update({partitionKey1: '', partitionKey2: ''})).type.toRaiseError();
         expect(testMapper.update({partitionKey1: '', partitionKey2: '', clustering1: ''})).type.toRaiseError();
+        expect(testMapper.update({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: 0})).type.toRaiseError();
 
         expect(testMapper.update({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: ''})).type.toBe<Promise<mapping.Result<TestRow>>>;
         expect(testMapper.update({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: '', extraColumn1: ''})).type.toBe<Promise<mapping.Result<TestRow>>>;
@@ -39,6 +43,7 @@ describe("Type tests", () => {
         expect(testMapper.remove({partitionKey1: '', partitionKey2: ''})).type.toRaiseError();
         expect(testMapper.remove({partitionKey1: '', partitionKey2: '', clustering1: ''})).type.toRaiseError();
         expect(testMapper.remove({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: '', extraColumn1: ''})).type.toRaiseError();
+        expect(testMapper.remove({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: 0})).type.toRaiseError();
 
         expect(testMapper.remove({partitionKey1: '', partitionKey2: '', clustering1: '', clustering2: ''})).type.toBe<Promise<mapping.Result<TestRow>>>;
     });
